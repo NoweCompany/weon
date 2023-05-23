695b3bd10f9389f5ee71a6a7a1246bce7c5f3118
geovaniorsoli <112351284+geovaniorsoli@users.noreply.github.com>
2023-05-23 10:53:17 -0300

    tentando arruma
    aa

--- assets/js/modules/logout.js
+++ assets/js/modules/logout.js
@@ -1,5 +1,5 @@
+const urlWebsite   = document.location.href
 const logaout      = document.querySelector('#Logaout') 
-
 logaout.addEventListener('click', (e) => {
     document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     window.location.assign(urlWebsite)
