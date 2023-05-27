163c7b0501a235a69bbbdb3ef0e4dbd866f808d0
Moyseys Veroni <moyseys.veroni@gmail.com>
2023-05-27 08:29:51 -0300

    bug msg erro login

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,7 +1,7 @@
 const form         = document.querySelector('.form');
 const inpEmail     = document.querySelector('#email');
 const inpPassword  = document.querySelector('#password');
-const conteinerMsg = document.querySelector('.msg')
+const containerMsg = document.querySelector('.msg')
 const url = 'https://instrutorcerto.com.br'
 const urlWebsite   = document.location.href.split('/').slice(0, -1).join('/')
 
@@ -38,14 +38,37 @@ class FormLogin{
       return true
     };
 
-    msg(msg){
-        conteinerMsg.classList.add('errors')
-        return conteinerMsg.innerHTML = msg
+    msg(msg, success){
+      if(!success){
+        containerMsg.className = 'error'
+        containerMsg.textContent = msg
+
+        setTimeout(() => {
+            this.cleanMsg()
+        }, 1000)
+
+        return
+    }else{
+        containerMsg.className = 'success'
+        containerMsg.textContent = msg
+
+        setTimeout(() => {
+            this.cleanMsg()
+        }, 1000);
+
+       return
+    }
     };
+
+    cleanMsg(){
+      containerMsg.innerHTML = ''
+      containerMsg.className = 'msg'
+      return
+    }
 
     async postApi(){
         try{
-          if(!this.validate()) return this.msg('Valores inválidos')
+          if(!this.validate()) return this.msg('Valores inválidos', false)
           const body = this.crateJsonBody()
           const response = await fetch(url + '/token', 
             {
@@ -60,14 +83,14 @@ class FormLogin{
           const data = await response.json();
           return data;
         }catch(e){
-            this.msg(e);
+            this.msg('Falha na conexão com o servidor', false);
         }
     };
 
     async init(){
       const response = await this.postApi();
       if(response.errors){
-        this.msg(response.errors);
+        this.msg(response.errors, false);
       }else{
         const token = response.token;
         const maxAge = 60*60*24*7
