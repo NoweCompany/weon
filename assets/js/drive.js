6b400abc147cd094a97c64b9426551d33d944726
geovaniorsoli <112351284+geovaniorsoli@users.noreply.github.com>
2023-05-27 21:28:41 -0300

    tabela pesquisa funcionadno
    funcionando a pesquisa

--- assets/js/drive.js
+++ assets/js/drive.js
@@ -1,46 +1,55 @@
-const url = 'https://instrutorcerto.com.br'
-const urlWebsite   = document.location.href
+
+
+const url = 'https://instrutorcerto.com.br';
+const urlWebsite = document.location.href;
 
 import Logado from "./modules/Logado.js";
 
 window.addEventListener('load', async (e) => {
-    const logado = new Logado()
-    if(!(await logado.userLogado())){
-      return window.location.assign(`${urlWebsite.split('/').slice(0, -2).join('/')}/index.html`)
-    }else{
-      return
-    }
-})
+  const logado = new Logado();
+  if (!(await logado.userLogado())) {
+    return window.location.assign(`${urlWebsite.split('/').slice(0, -2).join('/')}/index.html`);
+  } else {
+    return;
+  }
+});
 
-class Drive{
-  constructor(selectTable, containerTable, containerMsg, thead, tbody){
-    this.selectTable    = selectTable,
-    this.containerTable = containerTable,
-    this.containerMsg   = containerMsg,
-    this.thead          = thead,
-    this.tbody          = tbody
+class Drive {
+  constructor(selectTable, containerTable, containerMsg, thead, tbody) {
+    this.selectTable = selectTable;
+    this.containerTable = containerTable;
+    this.containerMsg = containerMsg;
+    this.thead = thead;
+    this.tbody = tbody;
+    this.tableData = [];
   }
 
-  async init(){
-    await this.addOptions()
-    this.events()
+  async init() {
+    await this.addOptions();
+    this.initSelect2();
+    this.events();
   }
 
-  async addOptions(){
+  async addOptions() {
     const headers = new Headers({
       "Content-Type": "application/json",
       "authorization": `Bearer ${this.token()}`
-    })
+    });
 
-    const response = await fetch(`${url}/template/table`,{
+    const response = await fetch(`${url}/template/table`, {
       method: 'GET',
       headers: headers
-    })
+    });
 
-    if(response.status !== 200) this.msg('Algo deu errado', false)
+    if (response.status !== 200) {
+      this.msg('Algo deu errado', false);
+      return;
+    }
 
-    const data = await response.json()
-    for(const key of data.response){
+    const data = await response.json();
+    this.tableData = data.response; // Salva os dados da tabela para uso posterior
+
+    for (const key of this.tableData) {
       const option = document.createElement('option');
       const textOption = document.createTextNode(key.tableName);
       option.appendChild(textOption);
@@ -48,122 +57,104 @@ class Drive{
     }
   }
 
-  token(){
+  token() {
     const token = document.cookie
       .split("; ")
       .find((row) => row.startsWith("token="))
       .split("=")[1];
-    return token 
+    return token;
   }
 
-  events(){
-    this.selectTable  .addEventListener('change', e =>  this.renderTable(e))
+  initSelect2() {
+    $(this.selectTable).select2();
   }
 
-  async renderTable(e){
-    this.thead.innerHTML = ''
-    this.tbody.innerHTML = ''
+  events() {
+    $(this.selectTable).on('change', (e) => this.renderTable(e.target.value));
+  }
 
-    const responseTables = await this.getTablesApi()
-    const dataTables = await responseTables.json()
+  async renderTable(selectedTable) {
+    this.thead.innerHTML = '';
+    this.tbody.innerHTML = '';
 
-    if(responseTables.status !== 200 ){
-      return this.msg(dataTables.errors)
+    const table = this.tableData.find((data) => data.tableName === selectedTable);
+    if (!table) {
+      this.msg('Tabela não encontrada.', false);
+      return;
     }
 
-    //procuro na response a tabela selecionado
-    for(const key in dataTables.response){
-      const optionSelected = e.target.value
-      const tableName = dataTables.response[key].tableName
-      //verifico é a tabela selecionado 
-      if(tableName === optionSelected){
-        //faço uma vrdura no array dentro da tabela para achar os campos
-        for(const indexField in dataTables.response[key].fields){
-          //monto a tabela
-          const th = document.createElement('th')
-          const fieldName = dataTables.response[key].fields[indexField].key
-          const textTh = document.createTextNode(fieldName)
-          th.appendChild(textTh)
-          this.thead.appendChild(th)
-        }
+    for (const field of table.fields) {
+      const th = document.createElement('th');
+      const textTh = document.createTextNode(field.key);
+      th.appendChild(textTh);
+      this.thead.appendChild(th);
+    }
 
-        const responseValues = await this.getVeluesApi(tableName)
-        const dataValues = await responseValues.json()
+    const responseValues = await this.getVeluesApi(table.tableName);
+    if (responseValues.status !== 200) {
+      const dataValues = await responseValues.json();
+      this.msg(dataValues.errors, false);
+      return;
+    }
 
-        if(responseValues.status !== 200 ){
-          return this.msg(dataValues.errors)
-        }
-        for(const field of dataValues){
-          const tr = document.createElement('tr')
-          for(const key in field){
-            const value = field[key]
-            const td = document.createElement('td')
-            const textTd= document.createTextNode(value)
+    const dataValues = await responseValues.json();
+    for (const field of dataValues) {
+      const tr = document.createElement('tr');
+      for (const key in field) {
+        const value = field[key];
+        const td = document.createElement('td');
+        const textTd = document.createTextNode(value);
 
-            td.appendChild(textTd)
-            tr.appendChild(td)
-            this.tbody.appendChild(tr)
-          }
-        }
+        td.appendChild(textTd);
+        tr.appendChild(td);
+        this.tbody.appendChild(tr);
       }
     }
-    
   }
 
-  async getVeluesApi(tableName){
+  async getVeluesApi(tableName) {
     const response = await fetch(`${url}/template/values/${tableName}`, {
       headers: {
         "Content-Type": "application/json",
         "authorization": `Bearer ${this.token()}`
       }
-    })
+    });
 
-    return response
+    return response;
   }
 
-  async getTablesApi(){
-    const response = await fetch(`${url}/template/table`, {
-      headers: {
-        "Content-Type": "application/json",
-        "authorization": `Bearer ${this.token()}`
-      }
-    })
+  msg(msg, success) {
+    if (!success) {
+      this.containerMsg.className = 'error';
+      this.containerMsg.textContent = msg;
 
-    return response
-  }
+      setTimeout(() => {
+        this.cleanMsg();
+      }, 1000);
 
-  msg(msg, success){
-    if(!success){
-        containerMsg.className = 'error'
-        containerMsg.textContent = msg
+      return;
+    } else {
+      this.containerMsg.className = 'success';
+      this.containerMsg.textContent = msg;
 
-        setTimeout(() => {
-            this.cleanMsg()
-        }, 1000)
+      setTimeout(() => {
+        this.cleanMsg();
+      }, 1000);
 
-        return
-    }else{
-        containerMsg.className = 'success'
-        containerMsg.textContent = msg
-
-        setTimeout(() => {
-            this.cleanMsg()
-        }, 1000);
-
-      return
+      return;
     }
   }
-  
-  cleanMsg(){
-    return containerMsg.className = 'msg'
-  } 
+
+  cleanMsg() {
+    this.containerMsg.className = 'msg';
+  }
 }
 
-const select         = document.querySelector('.selectTable')
-const containerTable = document.querySelector('.table')
-const thead          = document.querySelector('.thead')
-const tbody          = document.querySelector('.tbody')
-const containerMsg   = document.querySelector('.msg')
+const select = document.querySelector('.selectTable');
+const containerTable = document.querySelector('.table');
+const thead = document.querySelector('.thead');
+const tbody = document.querySelector('.tbody');
+const containerMsg = document.querySelector('.msg');
 
 const drive = new Drive(select, containerTable, containerMsg, thead, tbody);
 drive.init()
