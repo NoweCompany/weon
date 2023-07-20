a3147ee51d113535d5de7a9c7706d9089e8f55a3
NoweCompany <nowecompany@gmail.com>
2023-07-20 17:18:59 -0300

    mudanças nos links

--- weon/assets/js/modules/configs.js
+++ weon/assets/js/modules/configs.js
@@ -1,6 +1,6 @@
 export default {
     urlApi: 'https://apiweon.nowecompany.com.br',
-    urlWebsiteDefault: 'https://nowecompany/weon/',
+    urlWebsiteDefault: 'https://nowecompany.com.br/weon/',
     urlWebsiteRelativa: () => {
         const urlWebsite = document.location.href
         let urlFormated = urlWebsite.split("/").slice(0, -1).join('/')
