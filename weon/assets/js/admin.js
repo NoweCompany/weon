2935c5ce62a3486be44e7a56eb47cc960fea41e2
pedroka_3 <112211738+pedroka3@users.noreply.github.com>
2023-08-15 20:05:38 -0300

    lixeira (não finalizada)
    não consegui implementar o método getApiPresets

--- weon/assets/js/admin.js
+++ weon/assets/js/admin.js
@@ -24,10 +24,34 @@ class Admin {
             .split("; ")
             .find((row) => row.startsWith("token="))
             .split("=")[1];
+            
         return token
     }
 
+    async getItemsInTrash(collectionName) {
+        const headers = new Headers({
+            "Content-Type": "application/json",
+            "authorization": `Bearer ${this.token()}`
+        });
+
+        try {
+            const response = await fetch(`${url}/trash/${collectionName}`, {
+                headers: headers
+            });
+
+            if (response.status !== 200) {
+                const data = await response.json();
+                throw new Error(data.errors);
+            }
+
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            throw new Error("Erro ao obter itens da lixeira: " + error.message);
+        }
+    }
 }
+    
 
 class Preset extends Admin {
     async preset() {
@@ -648,13 +672,41 @@ class Fields extends Admin {
 
 }
 
+class Trash extends Admin{
+    async renderTrashItems() {
+        const trashItems = await super.getItemsInTrash('teste')
+        console.log(trashItems)
+
+        // Limpar o container antes de renderizar os itens
+        container.innerHTML = '';
+
+        // Renderizar os itens da lixeira
+        trashItems.forEach(value => {
+            for (const key in value) {
+                const element = value[key]
+                console.log(element)
+                const itemElement = document.createElement('div');
+                itemElement.innerHTML += element 
+
+                container.appendChild(itemElement);
+            }
+        })
+    }
+
+    // Chamar este método para exibir os itens da lixeira
+    trash() {
+        this.renderTrashItems();
+    }
+}
+
 const preset = new Preset()
 const fields = new Fields()
+const trash = new Trash()
 preset.preset()
 document.addEventListener('click', (e) => {
     const el = e.target
     const id = el.getAttribute('id')
     if (id === 'predefinicao') preset.preset()
     if (id === 'campos') fields.fields()
+    if (id === 'lixeira') trash.trash()
 })
-
