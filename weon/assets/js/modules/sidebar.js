29cdaf402deba70bbabc56c3d9be0625de5edb89
geovaniorsoli <112351284+geovaniorsoli@users.noreply.github.com>
2023-09-10 21:29:18 -0300

    side bar caramba

--- weon/assets/js/modules/sidebar.js
+++ weon/assets/js/modules/sidebar.js
@@ -1,56 +1,38 @@
-// Função para verificar o tamanho da tela e controlar o modo colapsado
-function checkWindowSize() {
-  var sidebar = document.querySelector('.sidebar');
-  var windowWidth = window.innerWidth;
-  var windowHeight = window.innerHeight;
-
-  if (windowWidth <= 1258 && windowHeight <= 848) { // Defina as dimensões limite para ativar o modo colapsado (por exemplo, 1258x848)
-    sidebar.classList.add('collapsed');
-    var title = document.getElementById('sidebar-title');
-    var buttons = document.querySelectorAll('.toggle-button');
-    var icons = document.querySelectorAll('.toggle-button i');
-    
-    title.classList.add('hidden');
-    
-    buttons.forEach(function (button) {
-      button.querySelector('i').classList.add('icon-hidden');
-    });
-  } else {
-    sidebar.classList.remove('collapsed');
-    var title = document.getElementById('sidebar-title');
-    var buttons = document.querySelectorAll('.toggle-button');
-    var icons = document.querySelectorAll('.toggle-button i');
-    
-    title.classList.remove('hidden');
-    
-    buttons.forEach(function (button) {
-      button.querySelector('i').classList.remove('icon-hidden');
-    });
-  }
-}
-
-// Execute a função quando a página for carregada e quando a janela for redimensionada
-window.addEventListener('load', checkWindowSize);
-window.addEventListener('resize', checkWindowSize);
-
-// Adicione o evento de clique para alternar o modo colapsado quando o botão for clicado
-document.getElementById('toggle-sidebar').addEventListener('click', function () {
+function activateSidebarToggle() {
+  var toggleSidebarButton = document.getElementById('toggle-sidebar');
   var sidebar = document.querySelector('.sidebar');
   var title = document.getElementById('sidebar-title');
-  var buttons = document.querySelectorAll('.toggle-button');
-  var icons = document.querySelectorAll('.toggle-button i');
-  
-  sidebar.classList.toggle('collapsed');
-  
-  if (sidebar.classList.contains('collapsed')) {
-    title.classList.add('hidden');
-    buttons.forEach(function (button) {
-      button.querySelector('i').classList.add('icon-hidden');
-    });
-  } else {
-    title.classList.remove('hidden');
-    buttons.forEach(function (button) {
-      button.querySelector('i').classList.remove('icon-hidden');
-    });
+  var predefinicaoButton = document.getElementById('predefinicao');
+  var isSidebarCollapsed = false;
+  var hasBeenActivated = false;
+
+  function toggleSidebar() {
+    if (predefinicaoButton.textContent.trim() === 'Predefinição') {
+      predefinicaoButton.innerHTML = '<i id="predefinicao" class="fa-solid fa-bars-staggered"></i>';
+    } else {
+      predefinicaoButton.innerHTML = 'Predefinição <i class="fa-solid fa-bars-staggered"></i>';
+    }
+
+    isSidebarCollapsed = !isSidebarCollapsed;
+    sidebar.classList.toggle('collapsed', isSidebarCollapsed);
+    title.classList.toggle('hidden', isSidebarCollapsed);
   }
-});
+  
+  toggleSidebarButton.addEventListener('click', toggleSidebar);
+
+  function checkSidebarState() {
+    var screenWidth = window.innerWidth;
+    var screenHeight = window.innerHeight;
+    if (screenWidth < 1297) {
+      if (!hasBeenActivated) {
+        toggleSidebar();
+        hasBeenActivated = true;
+      }
+    } else {
+      hasBeenActivated = false;
+    }
+  }
+  window.addEventListener('resize', checkSidebarState);
+  checkSidebarState();
+}
+activateSidebarToggle();
