83dca29df2faa6dae2f444e1903421ad3218e027
L. Guedes <88943346+vidacalura@users.noreply.github.com>
2023-09-16 19:37:32 -0300

    Update darkmode.js

--- weon/assets/js/modules/darkmode.js
+++ weon/assets/js/modules/darkmode.js
@@ -1,62 +1,60 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const darkModeSwitch = document.getElementById("darkModeSwitch") || null;
-    const htmlElement = document.documentElement;
-    let darkModeStylesheet;
+const darkModeSwitch = document.getElementById("darkModeSwitch") || null;
+const htmlElement = document.documentElement;
+let darkModeStylesheet;
 
-    function setLocalStorageItem(name, value) {
-        localStorage.setItem(name, value);
+function setLocalStorageItem(name, value) {
+    localStorage.setItem(name, value);
+}
+
+function getLocalStorageItem(name) {    
+    return localStorage.getItem(name) || "";
+}
+
+function applyLightTheme() {
+    htmlElement.setAttribute("data-bs-theme", "light");
+    if (darkModeStylesheet) {
+        darkModeStylesheet.remove();
     }
+    setLocalStorageItem("theme", "light");
+}
 
-    function getLocalStorageItem(name) {    
-        return localStorage.getItem(name) || "";
-    }
+function applyDarkTheme() {
+    htmlElement.setAttribute("data-bs-theme", "dark");
+    htmlElement.classList.add("dark-mode");
+    darkModeStylesheet = document.createElement("link");
+    darkModeStylesheet.rel = "stylesheet";
+    darkModeStylesheet.href = "../assets/css/darkmode.css";
+    document.head.appendChild(darkModeStylesheet);
+    setLocalStorageItem("theme", "dark");
+}
 
-    function applyLightTheme() {
-        htmlElement.setAttribute("data-bs-theme", "light");
-        if (darkModeStylesheet) {
-            darkModeStylesheet.remove();
-        }
-        setLocalStorageItem("theme", "light");
-    }
-
-    function applyDarkTheme() {
-        htmlElement.setAttribute("data-bs-theme", "dark");
-        htmlElement.classList.add("dark-mode");
-        darkModeStylesheet = document.createElement("link");
-        darkModeStylesheet.rel = "stylesheet";
-        darkModeStylesheet.href = "../assets/css/darkmode.css";
-        document.head.appendChild(darkModeStylesheet);
-        setLocalStorageItem("theme", "dark");
-    }
-
-    function applyThemeFromLocalStorage() {
-        const themeLocalStorage = getLocalStorageItem("theme");
-        if (darkModeSwitch) {
-            if (themeLocalStorage === "dark") {
-                applyDarkTheme();
-                darkModeSwitch.checked = true;
-            } else {
-                applyLightTheme();
-                darkModeSwitch.checked = false;
-            }
-        } else {
-            if (themeLocalStorage === "dark") {
-                applyDarkTheme();
-            } else {
-                applyLightTheme();
-            }
-        }
-    }
-
+function applyThemeFromLocalStorage() {
+    const themeLocalStorage = getLocalStorageItem("theme");
     if (darkModeSwitch) {
-        darkModeSwitch.addEventListener("change", function () {
-            if (darkModeSwitch.checked) {
-                applyDarkTheme();
-            } else {
-                applyLightTheme();
-            }
-        });
+        if (themeLocalStorage === "dark") {
+            applyDarkTheme();
+            darkModeSwitch.checked = true;
+        } else {
+            applyLightTheme();
+            darkModeSwitch.checked = false;
+        }
+    } else {
+        if (themeLocalStorage === "dark") {
+            applyDarkTheme();
+        } else {
+            applyLightTheme();
+        }
     }
+}
 
-    applyThemeFromLocalStorage();
-});
+if (darkModeSwitch) {
+    darkModeSwitch.addEventListener("change", function () {
+        if (darkModeSwitch.checked) {
+            applyDarkTheme();
+        } else {
+            applyLightTheme();
+        }
+    });
+}
+
+applyThemeFromLocalStorage();
