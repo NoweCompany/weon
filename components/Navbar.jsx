230314b7ee6ac10ca62017cf1b4d53af8f164d39
Geovani Orsoli <112351284+geovaniorsoli@users.noreply.github.com>
2024-02-06 07:59:31 -0300

    navbar route (frontend)
    mobile nao esta funcionando

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,37 +1,88 @@
-import { useState } from "react";
-import Link from "next/link";
-import sty from "../styles/style-components/navbar.module.css";
+import { useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import sty from '../styles/style-components/navbar.module.css';
 
-export default function NavBar() {
+export default function Nav() {
+    const router = useRouter()
     const [showDrawer, setShowDrawer] = useState(false);
 
     const toggleDrawer = () => {
         setShowDrawer(!showDrawer);
     };
 
+    const isActive = (pathname) => router.pathname === pathname ? sty.activeLink : '';
+
     return (
         <>
             <nav className={`${sty.navBody} ${showDrawer ? sty.open : ''}`}>
-                <img className={sty.navImg} src="/img/white.png" alt="" />
-                <Link className={sty.navItem} href="./home"></Link>
-                <Link className={sty.navItem} href="./docs">docs</Link>
-                <Link className={sty.navItem} href="./dash">dashboard</Link>
-                <Link className={sty.navItem} href="./admin">admin</Link>
-                <Link className={sty.navUser} href="./user">user</Link>
-                <div className={sty.menuIcon} onClick={toggleDrawer}>
-                <ion-icon name="menu-outline"></ion-icon>
+                <div className={sty.navItems}>
+                    <Link className={sty.navImg} href="/home"> <img src="/Img/white.png" alt="Logo" /></Link>
+                    <div className={sty.navItenContainer}>
+                        <div className={sty.navItem}>
+                            <Link href="/docs">
+                                <div className={isActive('/docs')}>docs</div>
+                            </Link>
+                        </div>
+
+                        <div className={sty.navItem}>
+                            <Link href="/dash">
+                                <div className={isActive('/dash')}>dash</div>
+                            </Link>
+                        </div>
+
+                        <div className={sty.navItem}>
+                            <Link href="/admin">
+                                <div className={isActive('/admin')}>admin</div>
+                            </Link>
+                        </div>
+
+                        <div className={sty.navUser}>
+                            <Link href="/user">
+                                <div className={isActive('/user')}>user</div>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </nav>
 
-            <div className={`${sty.drawer} ${showDrawer ? sty.open : ''}`}>
-                <a className={sty.drawerItem} href="">docs</a>
-                <a className={sty.drawerItem} href="">dashboard</a>
-                <a className={sty.drawerItem} href="">admin</a>
-                <a className={sty.drawerItem} href="">user</a>
-                <div className={sty.menuIcon} onClick={toggleDrawer}>
-                <ion-icon size="large" name="arrow-back-outline"></ion-icon>
+
+           <nav className={`${sty.drawer} ${showDrawer ? sty.open : ''}`}>
+                <div className={sty.navItems}>
+                    <Link className={sty.navImg} href="/home"> <img src="/Img/white.png" alt="Logo" /></Link>
+                    <div className={sty.navItenContainer}>
+                        <div className={sty.navItem}>
+                            <Link href="/docs">
+                                <div className={isActive('/docs')}>docs</div>
+                            </Link>
+                        </div>
+
+                        <div className={sty.navItem}>
+                            <Link href="/dash">
+                                <div className={isActive('/dash')}>dash</div>
+                            </Link>
+                        </div>
+
+                        <div className={sty.navItem}>
+                            <Link href="/admin">
+                                <div className={isActive('/admin')}>admin</div>
+                            </Link>
+                        </div>
+
+                        <div className={sty.navUser}>
+                            <Link href="/user">
+                                <div className={isActive('/user')}>user</div>
+                            </Link>
+                            <div className={sty.menuIcon} onClick={toggleDrawer}>
+                                <ion-icon size="large" name="arrow-back-outline"></ion-icon>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </nav>
+
+
         </>
     );
 }
