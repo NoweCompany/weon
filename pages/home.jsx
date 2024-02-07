981aec70990430b36f19b9cb4951b090e2e82d6c
Geovani Orsoli <112351284+geovaniorsoli@users.noreply.github.com>
2024-02-07 09:59:45 -0300

    home (front-end) concluido

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,9 +1,31 @@
 import NavBar from "../components/Navbar"
+import sty from "../styles/Style-Pages/home.module.css"
+import IconeNowe from "../components/IconeNowe";
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger} from "@/components/ui/sheet"
+import { Newspaper } from 'lucide-react';
 export default function home(){
+
     return(
         <>
         <NavBar />
-        <h1>helloworld</h1>
+        <div className={sty.containerCenter}>
+            <img className={sty.img} src="./img/black.png" alt="" />
+            <h1 className={sty.imgDescription}> Bem-vindo a weon</h1>
+        </div>
+
+        <Sheet>
+  <SheetTrigger> <p className={sty.link}> <Newspaper /> Updates </p> </SheetTrigger>
+  <SheetContent side={"bottom"}>
+    <SheetHeader>
+      <SheetTitle>ACOMPANHAR</SheetTitle>
+      <SheetDescription>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur ad, natus officiis pariatur non tempore vel sunt quasi adipisci eveniet dolore dolorum! Sit provident incidunt asperiores similique dignissimos, porro reprehenderit.
+      </SheetDescription>
+    </SheetHeader>
+  </SheetContent>
+</Sheet>
+
+<IconeNowe />
         </>
     )
 }
