31c281926f64dabe507f744b300aed72c0614d18
Geovani Orsoli <112351284+geovaniorsoli@users.noreply.github.com>
2024-02-08 14:12:16 -0300

    component floatnav (frontend)

--- components/FloatNav.jsx
+++ components/FloatNav.jsx
@@ -1,19 +1,34 @@
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 import sty from "../styles/style-components/floatnav.module.css";
 
-export default function FloatNav({ title }) {
+export default function FloatNav({ title, buttonContent, placeholderSelect, labelSelect, showSelect }) {
     return (
         <>
             <Card className={sty.card}>
                 <CardHeader className={sty.cardHeader}>
                     <CardTitle className={sty.cardTitle}>{title}</CardTitle>
-                    <div className={sty.buttonContainer}>
-                        <Button>Button</Button>
-                        <Button>Button</Button>
-                        <Button>Button</Button>
-                    </div>
                 </CardHeader>
+                <div className={sty.buttonContainer}>
+                    {buttonContent.map((content, index) => (
+                        <Button key={index}>{content}</Button>
+                    ))}
+                    {showSelect && (
+                        <Select>
+                            <SelectTrigger className="w-[180px]">
+                                <SelectValue placeholder={placeholderSelect}/>
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectLabel> {labelSelect} </SelectLabel>
+                                    <SelectItem value="download"> Baixar</SelectItem>
+                                    <SelectItem value="upload"> Upload</SelectItem>
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                    )}
+                </div>
             </Card>
         </>
     );
--- pages/docs.jsx
+++ pages/docs.jsx
@@ -1,9 +1,10 @@
 import NavBar from "../components/Navbar"
 import FloatNav from "../components/FloatNav"
 export default function docs(){
+    const buttonContent = ["inserir"];
     return(
         <>
-        <FloatNav title="Docs"> </FloatNav>
+        <FloatNav title="docs" buttonContent={buttonContent} placeholderSelect="Exportação" labelSelect="Execel" showSelect={false}/>
         <NavBar />
         <h1>docs</h1>
         </>
