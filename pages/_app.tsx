66e440d12861ee295de9b3e9ad74a8c7c1e7fc37
Geovani Orsoli <112351284+geovaniorsoli@users.noreply.github.com>
2024-02-21 16:11:10 -0300

    password ou word

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
-      <Toaster richColors={true}/>
+      <Toaster closeButton={true} richColors={true}/>
     </>
   );
 }
--- pages/test.tsx
+++ pages/test.tsx
@@ -1,10 +1,8 @@
 import NavBar from "../components/global/Navbar"
 import Sidebar from "../components/sidebar/AdminSidebar"
-import Alert from "../components/alert/AlertSonner"
 export default function test(){
           return(
         <>
-        <Alert />
         </>
     )
 }
