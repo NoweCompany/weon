303c3be625ae2759c1c7652c5dbe45c6b1d5da82
Geovani Orsoli <112351284+geovaniorsoli@users.noreply.github.com>
2024-02-27 10:37:14 -0300

    position mensage

--- services/Messaging.ts
+++ services/Messaging.ts
@@ -4,12 +4,12 @@ export default class Messaging{
   send(msg: string, success: boolean){
     if(success){
       toast.success(msg, {
-        position: 'top-center',
+        position: 'top-right',
       })
     }else{
       console.error(msg)
       toast.error(msg, {
-        position: 'top-center',
+        position: 'top-right',
       })
     }
   }
