8357dddf553cb87883b11275cd58ed535f65068e
Geovani Orsoli <112351284+geovaniorsoli@users.noreply.github.com>
2024-03-06 20:49:14 -0300

    button "voltar" set secondary

--- pages/admin/tables.tsx
+++ pages/admin/tables.tsx
@@ -65,6 +65,7 @@ export default function AdminTables() {
         {
             name: 'voltar',
             functionOnClick: onButtonClickBack,
+            variant: 'secondary'
         }
     ]
 
