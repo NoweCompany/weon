f57cef2a7681de40c68772b2e0a07df16f68d5a8
Geovani Orsoli <112351284+geovaniorsoli@users.noreply.github.com>
2024-03-11 16:37:24 -0300

    tentando ser amigo do bulid pt2

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,36 +1,36 @@
-// import TableListagem from "../components/adminComponents/TableListagem";
-// import { collection } from '@/apiRequests';
-// import { messaging } from "@/services";
-// import { useEffect, useState } from "react";
+import TableListagem from "../components/adminComponents/TableListagem";
+import { collection } from '@/apiRequests';
+import { messaging } from "@/services";
+import { useEffect, useState } from "react";
 
-// export default function Test() {
-//     const [collectionInfo, setCollectionInfos] = useState({})
+export default function Test() {
+    const [collectionInfo, setCollectionInfos] = useState({})
 
-//     useEffect(() => {
-//         getCollections()
-//     },[])
+    useEffect(() => {
+        getCollections()
+    },[])
     
-//     useEffect(()=>{
-//         console.log(collectionInfo)        
-//     },[collectionInfo])
+    useEffect(()=>{
+        console.log(collectionInfo)        
+    },[collectionInfo])
 
-//     function getCollections(){
-//         collection.getApi()
-//             .then((info: any[] | {error: string}) => {
+    function getCollections(){
+        collection.getApi()
+            .then((info: any[] | {error: string}) => {
               
-//               if(info && 'error' in info) return messaging.send(info.error, false)
+              if(info && 'error' in info) return messaging.send(info.error, false)
       
-//               setCollectionInfos(info)          
-//             })
-//             .catch((error) => {
-//               console.log(error)
-//               messaging.send(error, false)
-//             })
-//     }
+              setCollectionInfos(info)          
+            })
+            .catch((error) => {
+              console.log(error)
+              messaging.send(error, false)
+            })
+    }
 
-//     return (
-//         <>
-//         <TableListagem />
-//         </>
-//     );
-// }
+    return (
+        <>
+        {/* <TableListagem /> */}
+        </>
+    );
+}
